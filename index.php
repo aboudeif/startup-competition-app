3888e9d8ae7af8d359f325392128551ee8db8d4b
Aboudeif Abdallah <71098171+aboudeif@users.noreply.github.com>
2022-03-02 01:51:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -35,12 +35,12 @@
       $info_file = file_get_contents('json/competition.json');
       define('INFO', json_decode($info_file,true));
       
-      echo "<section class='about'><h2>".$info['title']."</h2><img src='".$info['logo']."' /><h2>عن المسابقة</h2><p>".$info['info']."</p></section>";
+      echo "<section class='about'><h2>".INFO['title']."</h2><img src='".INFO['logo']."' /><h2>".INFO['about']."</h2><p>".INFO['info']."</p></section>";
 
 	  
       # طباعة اسماء الشركات المسجلة في المسابقة
-      echo "<script>build(0,'".INFO['names']."');</script>";
-        echo "<section><div class='stage' id='_0'><h3>".INFO['names']."</h3></div>" ;
+      echo "<script>build(0,'".INFO['winners']."');</script>";
+        echo "<section><div class='stage' id='_0'><h3>".INFO['winners']."</h3></div>" ;
         $sub_counter = 1;
           foreach($data as $key => $value)
             echo "<div class='element' dir='rtl'><span>".$sub_counter++ ."- ". $key . " </span></div>";
@@ -49,10 +49,10 @@
       # الحلقة التكرارية المسؤولة عن التقييم المتكرر للشركات حتي فوز شركة واحدة
       for($winner = $data,$counter = 1,$success_rate = 50; $success_rate > 0; $success_rate /= 2){
         # استدعاء الدالة المسؤولة عن تقييم الشركات و اختيار الشركات الناجحة
-        $winner = successful_companies($winner,$success_rate);
-        $title = $winner['stage_title'];
-        echo "<script>build(".$counter.",'". $title."');</script>";
-        echo "<section><div class='stage' id='_".$counter."'><h3>". $title ." [ ".$counter++ ." ]</h3><h5>عتبة الفوز في هذه المرحلة أقل من  ".$success_rate."% </h5></div>" ;
+        $winner = evaluate_companies($winner,$success_rate);
+//         $title = $winner['stage_title'];
+        echo "<script>build(".$counter.",'". $winner['stage_title']."');</script>";
+        echo "<section><div class='stage' id='_".$counter."'><h3>". $winner['stage_title'] ." [ ".$counter++ ." ]</h3><h5>".INFO['threshold']."  ".$success_rate."% </h5></div>" ;
         $sub_counter = 1;
           foreach($winner as $key => $value){
             if($key == 'all_companies_failed' || $key == 'stage_title')
@@ -65,12 +65,12 @@
           unset($winner['all_companies_failed']);
           $winner_one = array_search(min($winner),$winner);
 
-        echo "<section><div class='stage' id='_".$counter++."'><h3> الشركة الفائزة </h3></div>" ;
+        echo "<section><div class='stage' id='_".$counter++."'><h3> ".INFO['winner']." </h3></div>" ;
             echo "<div class='element' dir='rtl'><span>&#11088; 1- ". $winner_one . " </span><small> %".min($winner)." </small></div>";
           echo "</section>";
 
-	echo "<script>build(-1,' رسالة التهنئة ');</script>";
-        echo "<section><div class='message' id='_-1'><div class='cup'>&#129351;</div> تهانينا لشركة <b>&nbsp;&nbsp;".$winner_one."&nbsp;&nbsp;</b>  &#11088;&#127881;الفوز بالمركز الأول &#9996 </div></section>";
+	echo "<script>build(-1,' ".INFO['message']." ');</script>";
+        echo "<section><div class='message' id='_-1'><div class='cup'>&#129351;</div> ".INFO['congrats']." <b>&nbsp;&nbsp;".$winner_one."&nbsp;&nbsp;</b>  &#11088;&#127881;".INFO['one']." &#9996 </div></section>";
         break;
         }
         else{
@@ -78,15 +78,15 @@
           unset($winner['all_companies_failed']);
           if(count($winner) == 1){ 
             
-	    echo "<script>build(-1,' رسالة التهنئة ');</script>";
-            echo "<section><div class='message' id='_-1'><div class='cup'>&#129351;</div> تهانينا لشركة <b>&nbsp;&nbsp;".key($winner)."&nbsp;&nbsp;</b>  &#11088;&#127881;الفوز بالمركز الأول &#9996 </div></section>";
+	    echo "<script>build(-1,' ".INFO['message']." ');</script>";
+            echo "<section><div class='message' id='_-1'><div class='cup'>&#129351;</div> ".INFO['congrats']." <b>&nbsp;&nbsp;".key($winner)."&nbsp;&nbsp;</b>  &#11088;&#127881;".INFO['one']." &#9996 </div></section>";
             break;
           }
         }
         
       }
 
-      function successful_companies($data,$success_rate){
+      function evaluate_companies($data,$success_rate){
         foreach($data as $key => $value){
           $data[$key] = rand(0,100);
           if($data[$key] < $success_rate)
@@ -102,8 +102,8 @@
           return $data;
         }
       }
-    echo "<section class='partner'><h2>الشركاء</h2><div class='container'>";
-    foreach($info['partner'] as $key=>$value)
+    echo "<section class='partner'><h2>".INFO['partners']."</h2><div class='container'>";
+    foreach(INFO['partner'] as $key=>$value)
     	echo "<span class='partner-item'><img src='".$value."' height='80px'><p>".$key."</p></span>";
     echo "</div></section>";
     ?>
